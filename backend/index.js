17e3c5c1531357182b93f9e61792d0c79d380b15
kasun-kalhara <kasunkalhara588@gmail.com>
2023-03-23 21:39:46 +0530

    frontend changes

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const server  = require ("./server.js");
 if(process.env.NODE_ENV === "test"){
   server.listen(3000, () => {
     console.log(
-      "Capstone Project Backend is running on http://localhost:3000"
+      "Capstone Project Backend is running on http://localhost:4401"
     );
   });
 }else{
